0fe7f59b52620981609346221267e88c5c132030
Serhat Yildiz <yildizserhat@yahoo.com>
2025-10-17 14:38:48 +0200

    changes

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,37 +1,10 @@
-import { useNavigate, Link } from "react-router-dom";
-import { UserContext } from "../context/UserContext";
-import { useContext } from "react";
-
-export default function Header()
-{
-    // ------------------Variables------------------
-
-    const loggedData = useContext(UserContext);
-
-    const navigate = useNavigate();
-
-
-    // ------------------Functions------------------
-
-    function logout()
-    {
-        localStorage.removeItem("app-user");
-        loggedData.setLoggedUser(null);
-        navigate("/login");
-
-    }
 
 
 
-    return(
-        <div>
-             <ul className="header">
-                {/* <Link to="/diet"><li>Diyet</li></Link>
-                <Link to="/search"><li>Arama</li></Link>
-                <Link to="/createfood"><li>Oluştur</li></Link>
-                <Link to="/weight"><li>Kilo</li></Link>
-                <li onClick={logout}>Çıkış</li> */}
-            </ul>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="notch-header">
+      {/* Optional: Add a logo or leave it blank to just push content below the notch */}
+    </div>
+  );
 }
